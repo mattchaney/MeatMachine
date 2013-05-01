3ec8df9bf60dbbe2e79013355d5916638883a962
Matt Chaney <chaney.mathew@gmail.com>
2013-05-01 16:36:05 -0400

    using BeautifulSoup now...

--- kolbot.py
+++ kolbot.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import re, requests, hashlib, urllib, urllib2, cookielib
+import requests, hashlib
 from bs4 import BeautifulSoup
 from sys import argv
 
@@ -12,25 +12,10 @@ class UrlError(Exception):
 
 def login(s):
 	r = s.get('http://www.kingdomofloathing.com')
-
-	loginUrlPattern = re.compile(r'^(.*)login\.php\?loginid=([0-9a-f]+)')
-	serverMatch = loginUrlPattern.search(r.url)
-	if serverMatch:
-		serverURL = serverMatch.group(1)
-		print 'serverURL:', serverURL
-	else:
-		raise UrlError('Error getting serverURL')
-
-	loginChallengePattern = re.compile(r'name="?challenge"?\s+value="?([0-9a-f]+)"?')
-	challengeMatch = loginChallengePattern.search(r.text)
-	if challengeMatch:
-		challenge = challengeMatch.group(1)
-		print 'challenge:',challenge
-	else:
-		raise UrlError('No match to challenge pattern')
-
+	page = BeautifulSoup(r.text)
+	serverURL = r.url
 	loginID = serverURL[51:]
-	
+	challenge = page.find('input', attrs = {'name':'challenge'})['value']
 	userName = s.auth[0]
 	pwdHash = hashlib.md5(s.auth[1]).hexdigest()
 	hashKey = pwdHash + ":" + challenge
@@ -46,6 +31,7 @@ def login(s):
 
 	r = s.post('http://www.kingdomofloathing.com/login.php', data=form_data)
 	print r.url
+	return r
 
 def scrounge(s):
 	form_data = {
@@ -59,6 +45,7 @@ def main():
 	script, username, password = argv
 	s = requests.Session()
 	s.auth = (username, password)
-	login(s)
-
+	r = login(s)
+	output = open('output', 'w')
+	output.write(r.text)
 main()
