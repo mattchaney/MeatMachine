baa9a4f6e4a5813d7dbd0995c5a78b7bcd63b203
Matt Chaney <chaney.mathew@gmail.com>
2013-07-10 16:54:49 -0400

    Added food and booze consumption

--- MeatMachine.py
+++ MeatMachine.py
@@ -11,6 +11,21 @@ class MeatError(Exception):
 
 class MeatMachine(object):
 	def __init__(self):
+		self.items = {
+			#food
+			'insanely spicy bean burrito':316,
+			'peach pie':2769,
+			#booze components
+			'magical ice cubes':1008,
+			'coconut shell':1007,
+			'cocktail onion':1560,
+			'raspberry':1561,
+			#booze
+			'bottle of domesticated turkey':1551,
+			'bottle of definit':1552,
+			'bottle of calcutta emerald':1553,
+			'boxed champagne':1556,
+		}
 		self.session = requests.session()
 		self.loggedin = False
 
@@ -21,35 +36,34 @@ class MeatMachine(object):
 		with an appropriate message. 
 		This must be called before any other actions can be taken with the bot.
 		'''
-		if not self.loggedin:
-			username = raw_input('Username: ')
-			password = getpass.getpass()
-			self.session.auth = (username, password)
-			self.serverURL = 'http://www.kingdomofloathing.com'
-			response = self.session.get(self.serverURL)
-			page = BeautifulSoup(response.text)
-			loginID = response.url[51:]
-			pwdHash = hashlib.md5(password).hexdigest()
-			challenge = page.find('input', attrs = {'name':'challenge'})['value']
-			hashKey = pwdHash + ":" + challenge
-			response = hashlib.md5(hashKey).hexdigest()
-			form_data = {
-				'loggingin':'Yup.',
-				'loginname':username,
-				'secure':'1',
-				'challenge':challenge,
-				'response':response
-			}
-			response = self.session.post(self.serverURL + '/login.php', data=form_data)
-			# self.output('login', response.text)
-			if 'login' in response.url:
-				raise MeatError('Couldn\'t connect... for some reason')
-			else:
-				self.loggedin = True
-				print 'You are now logged in'
-				self.update()
-		else:
+		if self.loggedin:
 			return
+		username = raw_input('Username: ')
+		password = getpass.getpass()
+		self.session.auth = (username, password)
+		self.serverURL = 'http://www.kingdomofloathing.com'
+		response = self.session.get(self.serverURL)
+		page = BeautifulSoup(response.text)
+		loginID = response.url[51:]
+		pwdHash = hashlib.md5(password).hexdigest()
+		challenge = page.find('input', attrs = {'name':'challenge'})['value']
+		hashKey = pwdHash + ":" + challenge
+		response = hashlib.md5(hashKey).hexdigest()
+		form_data = {
+			'loggingin':'Yup.',
+			'loginname':username,
+			'secure':'1',
+			'challenge':challenge,
+			'response':response
+		}
+		response = self.session.post(self.serverURL + '/login.php', data=form_data)
+		# self.output('login', response.text)
+		if 'login' in response.url:
+			print 'Couldn\'t connect... for some reason'
+		else:
+			self.loggedin = True
+			print 'You are now logged in'
+			self.update()
 
 	def logout(self):
 		'''
@@ -75,21 +89,21 @@ class MeatMachine(object):
 		'''
 		Must be logged in before this method is called.
 		'''
-		if self.loggedin:
-			payload = {'what':'status', 'for':'MeatMachine by Moot'}
-			response = self.session.get(self.serverURL + '/api.php', params=payload)
-			data = json.loads(response.text)
-			self.pwd = data['pwd']
-			self.hp = int(data['hp'])
-			self.maxhp = int(data['maxhp'])
-			self.mp = int(data['mp'])
-			self.meat = int(data['meat'])
-			self.adventures = int(data['adventures'])
-			# payload = {'what':'inventory', 'for':'MeatMachine by Moot'}
-			# response = self.session.get(self.serverURL + '/api.php', params=payload)
-			# data = json.loads(response.text)
-		else:
-			raise MeatError("You must log in before calling update")
+		if not self.loggedin:
+			raise MeatError("def update: You must log in before calling update")
+		payload = {'what':'status', 'for':'MeatMachine by Moot'}
+		response = self.session.get(self.serverURL + '/api.php', params=payload)
+		data = json.loads(response.text)
+		self.pwd = data['pwd']
+		self.hp = int(data['hp'])
+		self.maxhp = int(data['maxhp'])
+		self.mp = int(data['mp'])
+		self.meat = int(data['meat'])
+		self.drunk = int(data['drunk'])
+		self.adventures = int(data['adventures'])
+		payload = {'what':'inventory', 'for':'MeatMachine by Moot'}
+		response = self.session.get(self.serverURL + '/api.php', params=payload)
+		self.invenvtory = json.loads(response.text)
 
 	def adventure(self, where):
 		'''
@@ -100,19 +114,18 @@ class MeatMachine(object):
 		refer to http://kol.coldfront.net/thekolwiki/index.php/Areas_by_Number
 		'''
 		if not self.loggedin:
-			raise MeatError('You must log in before calling adventure()')
+			raise MeatError('def adventure: You must log in before calling adventure()')
 		if(self.adventures == 0):
 			print "You're out of adventures"
 			return
 		if not isinstance(where, int):
-			raise MeatError('Adventure location must be an integer')
+			raise MeatError('def adventure: Adventure location must be an integer')
 
 		snarfblat = where
 		adventureURL = self.serverURL + '/adventure.php'
 		payload = {'snarfblat':where}
 		response = self.session.get(adventureURL, params=payload)
 		page = BeautifulSoup(response.text)
-		# If this is a non-combat adventure, just return
 		if 'Adventure Again' in response.text:
 			self.update()
 			return
@@ -139,11 +152,11 @@ class MeatMachine(object):
 		refer to http://kol.coldfront.net/thekolwiki/index.php/Skills_by_number
 		'''
 		if not self.loggedin:
-			raise MeatError('You must log in before calling use_skill()')
+			raise MeatError('def use_skill: You must log in before calling use_skill()')
 		if not isinstance(what, int):
-			raise MeatError('Skill id must be an integer')
+			raise MeatError('def use_skill: Skill id must be an integer')
 		if quantity < 1:
-			raise MeatError('Can\'t use this skill a negative quantity of times: too meta')
+			raise MeatError('def use_skill: Can\'t use this skill a negative quantity of times: too meta')
 		skill = what
 		form_data = {
 			'pwd':self.pwd,
@@ -155,17 +168,47 @@ class MeatMachine(object):
 		# self.output('skill',response.text)
 		self.update()
 
-	def consume(self, what):
+	def consume(self, type, which_item, how_many=1):
 		'''
-		Will look in player inventory for the target item 'what' and 
+		Will look in player inventory for the target item 'which_item' and 
 		eat or drink it if it's available. Must be fed an integer item id number to work.
 		For a list of valid item IDs, 
 		refer to http://kol.coldfront.net/thekolwiki/index.php/Items_by_number
 		'''
-		pass
+		if not self.loggedin:
+			raise MeatError('def consume: You must log in first')
+		if not isinstance(which_item, int) or not isinstance(how_many, int):
+			raise MeatError('def consume: Parameter how_many and which_item must be of type integer')
+		if how_many < 1:
+			raise MeatError('def consume: Can\'t use this skill a negative quantity of times')
+		form_data = {
+			'pwd': self.pwd,
+			'which': how_many,
+			'whichitem': which_item
+		}
+		if type is 'food':
+			response = self.session.post(self.serverURL + '/inv_eat.php', data=form_data)
+		elif type is 'booze':
+			response = self.session.post(self.serverURL + '/inv_booze.php', data=form_data)
+		else:
+			raise MeatError('Type must be either food or booze')
+		self.output('consume', response.text)
+		self.update()
 
-	def make(self, what):
+	def craft(self, what, a, b):
 		'''
-		Will attempt to craft the item specified by the item ID
+		Will attempt to craft the type of item specified by the param 'what'. 
+		'what' should be 'cocktail' to craft a booze item.
 		'''
-		pass
+		# if not self.loggedin:
+		# 	raise MeatError('Must be logged in')
+		# form_data = {
+		# 	'mode':what,
+		# 	'a':get_id(a),
+		# 	'b':get_id(b)
+		# }
+		# response = self.session.post(self.serverURL + '/craft', data=form_data)
+		# self.output('craft', response.text)
+
+	def get_id(self, item_name):
+		return self.items[item_name]
--- main.py
+++ main.py
@@ -9,14 +9,12 @@ heals for 40 hp) three times.
 '''
 from MeatMachine import MeatMachine
 
-# 51 = Menagerie lvl1, 53 = Menagerie lvl2, 110 = icy peak 
 bot = MeatMachine()
 bot.login()
-bot.use_skill(5014, 3) # Advanced Cocktailcrafting
-for x in range(15):
-	bot.adventure(51)
-for x in range(15):
-	bot.adventure(53)
+bot.use_skill(5014, 5) # Advanced Cocktailcrafting
+for _ in xrange(5):
+	bot.consume('food', bot.get_id('insanely spicy bean burrito'))
+
 while bot.has_adventures():
 	if bot.hp < 40:
 		bot.use_skill(5011, 2)
