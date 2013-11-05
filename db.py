c2157fe16d7013747b1bbf8f083d14083694a8e5
Matt Chaney <chaney.mathew@gmail.com>
2013-11-05 16:17:12 -0500

    added db for item name to id map

---
+++ db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+items = {
+	#food
+	'insanely spicy bean burrito':316,
+	'peach pie':2769,
+	#booze components
+	'olive':245,
+	'strawberry':786,
+	'magical ice cubes':1008,
+	'coconut shell':1007,
+	'cocktail onion':1560,
+	'raspberry':1561,
+	#booze
+	'bottle of gin':237,
+	'bottle of vodka':238,
+	'bottle of whiskey':328,
+	'bottle of rum':787,
+	'bottle of tequila':1004,
+	'boxed wine':1005,
+	'bottle of domesticated turkey':1551,
+	'bottle of definit':1552,
+	'bottle of calcutta emerald':1553,
+	'boxed champagne':1556,
+	'parisian cathouse':1561,
+	'vodka gibson':1569,
+	'rabbit punch':1572,
+	'vodka stratocaster':1581,
+	'neuromancer':1582,
+	'mae west':1584
+}
